4f43375901494a334f8be4f30acf9163575514d8
Gelev97 <geruohai97@outlook.com>
2025-10-01 12:45:44 -0700

    remove the crew

--- bga_stats.py
+++ bga_stats.py
@@ -37,8 +37,8 @@ class BGAGameTracker:
 		self._password = os.environ.get("BGA_PASSWORD")
 		self._base_url = "https://boardgamearena.com"
 
-		# Game IDs to exclude (Turing Machine, Hanabi, wordtraveler, sail, skyteam, daybreak)
-		self._exclude_game_ids = ["1015", "1804", "1937", "2092", "1879", "1799"]
+		# Game IDs to exclude (Turing Machine, Hanabi, wordtraveler, sail, skyteam, daybreak, thecrew)
+		self._exclude_game_ids = ["1015", "1804", "1937", "2092", "1879", "1799", "1248"]
 		self._session = requests.Session()
 		self._request_token = None
 		self._user_ids = []
